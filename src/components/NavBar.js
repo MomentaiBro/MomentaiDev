736c5a643c482dcef8dc7478828021203c143316
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 09:49:47 -0600

    updated navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,28 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 export default function NavBar() {
     return(
-        <h1>NavBar Page</h1>
+        <header>
+            <div>
+                <nav>
+                    <NavLink>
+                        Home
+                    </NavLink>
+                    <NavLink>
+                        About
+                    </NavLink>
+                    <NavLink>
+                        
+                    </NavLink>
+                    <NavLink>
+                        Home
+                    </NavLink>
+                    <NavLink>
+                        Contact
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
     )
 }
