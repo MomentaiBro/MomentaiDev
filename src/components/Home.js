7acc6665bf1cbac0e39ea48138a1be171ba51cc0
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 14:02:16 -0600

    oops more stylin lol

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function Home() {
     return(
-        <h1 className="home__title title">Home Page</h1>
+        
+        <main>
+            
+            <img/>
+            <section>
+                <h1 className="home__title title">Home Page</h1>
+            </section>
+            
+        </main>
     )
 }
