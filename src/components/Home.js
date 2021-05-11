2b7a93780448a78e900cfd0d607d244a4682efc1
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 14:19:16 -0600

    zomg more styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from "react"
+import image from "../1133210.jpeg"
 
 export default function Home() {
     return(
         
-        <main>
-            
-            <img/>
+        <main className="home__wrapper">
             <section>
                 <h1 className="home__title title">Home Page</h1>
             </section>
+            <img src={image} alt="Background image from the anime show Gurren Lagen"/>
+            
             
         </main>
     )
