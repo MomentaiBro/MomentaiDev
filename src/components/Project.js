5533c6c5d7baa0133e3f454802d99c3dcb5b28ed
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 16:50:26 -0600

    got projects working

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React, { useEffect, UseState } from "react"
+import React, { useEffect, useState } from "react"
 import sanityClient from "../client"
 
 export default function Project() {
@@ -6,7 +6,7 @@ export default function Project() {
     const [projectData, setProjectData] = useState(null)
 
     useEffect(() => {
-        sanityClient.fetch(`*[_type == "project]{
+        sanityClient.fetch(`*[_type == "project"]{
             title,
             date,
             place, 
@@ -23,17 +23,47 @@ export default function Project() {
             <section>
                 <h1 className="project__title title">Project Page</h1>
                 <h2>These are my Projects</h2>
+
+        {projectData && projectData.map((project, index) => (
+
                 <article>
                     <h3>
-                        <div>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <p></p>
-                            <span></span>
-                        </div>
+                        <a
+                        href={project.link}
+                        alt={project.title}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                            {project.title}
+                        </a>
                     </h3>
+                        <div>
+                            <span>
+                                <strong>Finished on</strong>:{" "}
+                                {new Date(project.date).toLocaleDateString()}
+                            </span>
+                            <span>
+                                <strong>Company</strong>:{" "}
+                                {project.place}
+                            </span>
+                            <span>
+                                <strong>Type</strong>:{" "}
+                                {project.projectType}
+                            </span>
+                            <p> {project.description}</p>
+                            <a
+                            href={project.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                                View the Project{" "}
+                            <span role="img" aria-label="right pointer">
+                                👉
+                            </span>
+                            </a>
+                        </div>
                 </article>
+                ))}
             </section>
         </main>
 
