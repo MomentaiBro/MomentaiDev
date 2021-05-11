6ab0867b2a9ff80912cbbd49c4f2e35e1f1c9db5
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 16:30:04 -0600

    additional updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,41 @@
-import React from "react"
+import React, { useEffect, UseState } from "react"
+import sanityClient from "../client"
 
 export default function Project() {
+
+    const [projectData, setProjectData] = useState(null)
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "project]{
+            title,
+            date,
+            place, 
+            description,
+            projectType,
+            link,
+            tags
+        }`)
+        .then((data) => setProjectData(data))
+        .catch(console.error)
+    }, []);
     return(
-        <h1 className="project__title title">Project Page</h1>
+        <main className="project__main">
+            <section>
+                <h1 className="project__title title">Project Page</h1>
+                <h2>These are my Projects</h2>
+                <article>
+                    <h3>
+                        <div>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <p></p>
+                            <span></span>
+                        </div>
+                    </h3>
+                </article>
+            </section>
+        </main>
+
     )
 }
