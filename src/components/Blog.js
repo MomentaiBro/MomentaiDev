0577992ce134ff56d6347627524fb814d470c95e
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 14:39:16 -0600

    checkpoint

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,27 @@
-import React from "react"
+import React, {useState, useEffect } from "react"
+import sanityClient from "../client.js"
 
 export default function Blog() {
+
+    const [postData, setPost] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "post"]{
+                title,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    },
+                    alt
+                }
+            }`)
+            .then((data) => setPost(data))
+            .catch(console.log(error))
+    })
+
+
     return(
         <h1 className="blog__title title">Blog Page</h1>
     )
