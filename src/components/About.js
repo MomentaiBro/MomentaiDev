9a48e73f5f70ecfdeacee2487420c2045ec58aa4
Alexander Sherwin <alx.sherwin@gmail.com>
2021-05-11 21:40:00 -0600

    initial about me structure built

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,43 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client"
+import imageUrlBuilder from "./@sanity/image-url"
+
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function About() {
+
+    const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, []);
+
+    if(!author) return <div> Loading...</div>
+
     return(
-        <h1 className="about__title title">About Page</h1>
+
+        <main>
+            <img />
+            <div>
+                <section>
+                    <img src={uerlFor(author.authorImage).url()}/>
+                    <div>
+                        <h1 className="about__title title">About Page</h1>
+                        <div>
+                            BLOCK CONTENT
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
     )
 }
